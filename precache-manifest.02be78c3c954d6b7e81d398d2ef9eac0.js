0b9ff1d1ca1deacac30ff92b5a905b74da9ad31b
olektymchenko <58951697+olektymchenko@users.noreply.github.com>
2020-04-03 16:20:43 +0200

    Updates

---
+++ precache-manifest.02be78c3c954d6b7e81d398d2ef9eac0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c98071d47f30ac6ae3c1e69db0d434",
+    "url": "/culinary/index.html"
+  },
+  {
+    "revision": "b67e6857b7e002e51128",
+    "url": "/culinary/static/css/main.ad78fcc5.chunk.css"
+  },
+  {
+    "revision": "5fe7d02a7050d0514631",
+    "url": "/culinary/static/js/2.e8dbd55f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/culinary/static/js/2.e8dbd55f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b67e6857b7e002e51128",
+    "url": "/culinary/static/js/main.d043d02d.chunk.js"
+  },
+  {
+    "revision": "353a9a6243436ed49eaf",
+    "url": "/culinary/static/js/runtime-main.c79d2d26.js"
+  },
+  {
+    "revision": "4dd1c615b5ea8ecc76cd476dd111f219",
+    "url": "/culinary/static/media/utensils-solid.4dd1c615.svg"
+  }
+]);
